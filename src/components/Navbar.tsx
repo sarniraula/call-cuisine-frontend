783192ba916444f47969a7626eafb0fa095ea6db
sarniraula <sar.niraula162@gmail.com>
2025-04-09 10:07:46 -0400

    Added brand logo, color.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,20 @@
+import { useRouter } from 'next/navigation';
+
 export default function Navbar() {
+    const router = useRouter();
     return (
       <div className="bg-white shadow-md p-4 flex items-center justify-between">
-        {/* Logo Placeholder */}
-        <div className="text-2xl font-bold">Call Cuisine</div>
+        <img src="/logo-text.png" alt="Call Cuisine Logo" className="w-40" />
+
   
         {/* Future Actions (User Profile, Logout, etc.) */}
         <div className="flex items-center space-x-4">
-          <button className="px-4 py-2 bg-gray-200 rounded-md">Profile</button>
+          <button onClick={() => {
+              localStorage.clear();
+              router.push('/login');
+              window.location.reload(); 
+          }} 
+          className="px-4 py-2 bg-gray-200 rounded-md">Logout</button>
         </div>
       </div>
     );
